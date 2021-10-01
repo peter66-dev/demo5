2fee97742215c99a29c2e6c509efbdfa7630e401
phuxiteo3 <74146862+phuxiteo3@users.noreply.github.com>
2021-10-01 09:12:59 +0700

    Add files via upload

---
+++ src/demo/Demo6.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo;
+import java.util.List;
+/**
+ *
+ * @author Dell
+ */
+public class Demo6 {
+
+    public static void main(String[] args) {
+        int a, b;
+        a = 3;
+        b = 5;
+        int result1 = a + b;
+        int result2 = a - b;
+        int result3 = a * b;
+        int result4 = a / b;
+
+        System.out.println(result1);
+    }
+}
